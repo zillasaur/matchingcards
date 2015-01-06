612044bccd7d8c08d81e4af95a5041c10a95cf6d
zillasaur <ziyuechen12@gmail.com>
2015-01-06 17:40:47 -0600

    Initial Commit

---
+++ matchingcards/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  matchingcards
+//
+//  Created by Ziyue Chen on 1/6/15.
+//  Copyright (c) 2015 Ziyue Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ matchingcards/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  matchingcards
+//
+//  Created by Ziyue Chen on 1/6/15.
+//  Copyright (c) 2015 Ziyue Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
